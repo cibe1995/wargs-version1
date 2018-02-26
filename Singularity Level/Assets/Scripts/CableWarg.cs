4b22c521e21a5ee164090cb699eed371986363d8
MorganOtt <morgan.a.ott@gmail.com>
2018-02-26 16:32:14 -0500

    Battery Warg Created
    Player picks up red cube and places on top of battery switches the scene

---
+++ Singularity Level/Assets/Scripts/CableWarg.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CableWarg : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+
+        
+		
+	}
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("battery"))
+        {
+            Debug.Log("Zappppppp");
+
+            SceneManager.LoadScene(1);
+            
+
+        }
+    }
+}
