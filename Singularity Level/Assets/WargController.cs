8a25179a749936812640d59194aca7fc2be885b1
MorganOtt <morgan.a.ott@gmail.com>
2018-04-03 16:35:01 -0400

    Tweaked Chair for Pull Down Effect

---
+++ Singularity Level/Assets/WargController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WargController : MonoBehaviour {
+
+	private Quaternion rotation = Quaternion.Euler(new Vector3(180,-19,-90));
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//	Debug.Log (rotation);
+		transform.rotation = rotation;
+
+	}
+
+	void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("headset"))
+        {
+            Debug.Log("Zappppppp");
+
+            SceneManager.LoadScene(1);
+            
+
+        }
+    }
+}
